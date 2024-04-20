331fe135538914588e80de2495efc4b99f0b84c8
Raghuvarma-cloud <152680435+Raghuvarma-cloud@users.noreply.github.com>
2024-04-20 11:49:30 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.25.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "my-project-id" #replace with your GCP organisation project ID
+  region      = "us-central1"
+}
